347b8725b7a0b82a7f067b59c5fd241d28f25a04
BT_C09_16 <mobileappdev@bths.edu>
2026-04-22 09:25:04 -0400

    added sampleview

---
+++ GitTut/SampleView.swift
@@ -0,0 +1,18 @@
+//
+//  SampleView.swift
+//  GitTut
+//
+//  Created by Student on 4/22/26.
+//
+
+import SwiftUI
+
+struct SampleView: View {
+    var body: some View {
+        Text("ts mad fire")
+    }
+}
+
+#Preview {
+    SampleView()
+}
